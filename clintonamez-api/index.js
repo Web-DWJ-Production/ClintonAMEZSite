85c42fe014dbd972d6dbda38dadf758bd94ddd97
Alex Goley <ajgoley@gmail.com>
2018-02-15 20:38:47 -0500

    Initial commit with deployment configuration.

---
+++ clintonamez-api/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => {
+      console.log(`Listening on ${ PORT }`);
+
+      // Create the app-initialized file to signal to NGINX the api is ready.
+      fs.openSync('/tmp/app-initialized', 'w');
+  })
