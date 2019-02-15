784759ffe082061e262ac52008e93a2f9a32cacb
reddingk <kris.redding3@gmail.com>
2019-02-15 07:36:59 -0500

    update

--- src/js/templates/ministries.js
+++ src/js/templates/ministries.js
@@ -25,19 +25,66 @@ class MinistryAll extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            ministryList:[
+                {sectionTitle:"Section 1", list:[
+                    {title:"Ministry 1", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 2", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 3", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry Long Name 4", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 5", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 6", logo:null, subSections:[]},
+                    {title:"Ministry 7", logo:null, subSections:[]},
+                    {title:"Ministry 8", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry Long Name 9", logo:null, subSections:[]}
+                ]},
+                {sectionTitle:"Section 2", list:[
+                    {title:"Ministry 1", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry Long Name 2", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 3", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry Long Name 4", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 5", logo:null, subSections:["test subsection"]}
+                ]},
+                {sectionTitle:"Section 3", list:[
+                    {title:"Ministry Long Name 1", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 2", logo:null, subSections:["test subsection Long Name "]},
+                    {title:"Ministry 3", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 4", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 5", logo:null, subSections:["test subsection"]},
+                    {title:"Ministry 6", logo:null, subSections:[]},
+                    {title:"Ministry 7", logo:null, subSections:[]},
+                    {title:"Ministry Long Name 8", logo:null, subSections:["test subsection"]}
+                ]}
+            ]
+        }
     } 
 
     render(){        
         return(
-            <div class="site-page ministries">
-                <section class="title-card c1">
-                    <h1 class="font-title1">Ministries</h1>
-                    <div class="back-img">
-                        <div class="cover c3c2"></div>
+            <div className="site-page ministries">
+                <section className="title-card c1">
+                    <h1 className="font-title1">Ministries</h1>
+                    <div className="back-img">
+                        <div className="cover c3c2"></div>
                         <img src={backImg} />
                     </div>
                 </section>
+                {this.state.ministryList.map((msection,i) =>
+                    <section className={"body-section notched-top" + (i%2==1? " c2-mid" : "")}>
+                        <h1 className="font-title1">{ msection.sectionTitle }</h1>
+                        <div className="ministry-container">
+                            {msection.list.map((ministry,j) =>
+                                <a href="">
+                                    <div className="tag-img"><img src={ministry.icon} alt="minstry icon"/></div>
+                                    <div className="tag-info">
+                                        <div className="tag-title">{ministry.title}</div>
+                                        {ministry.subSections.length > 0 && <div className="tag-subsection">{ministry.subSections[0]}</div>}
+                                    </div>
+                                </a>
+                            )}
+                        </div>
+                    </section>                
+                )}                
             </div>    
         );        
     }
