7924f5f9a0020f0aaaad78df92d1e27b44890624
reddingk <kris.redding3@gmail.com>
2020-08-05 11:23:24 -0400

    switch to base app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ComingSoon from './js/comingSoon.js';
-//import App from './js/app.js';
+//import ComingSoon from './js/comingSoon.js';
+import App from './js/app.js';
 
-/* ReactDOM.render(<App />, document.getElementById('app')); */
-ReactDOM.render(<ComingSoon />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app')); 
+//ReactDOM.render(<ComingSoon />, document.getElementById('app'));
