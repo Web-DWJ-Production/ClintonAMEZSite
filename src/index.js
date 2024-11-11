7171421268dd51c5bac4a18218ad0cd5a09c7b60
reddingk <kris.redding3@gmail.com>
2024-11-11 10:12:51 -0500

    toggle on site

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import ComingSoon from './js/comingSoon.js';
-//import App from './js/app.js';
+import App from './js/app.js';
 
-//ReactDOM.render(<App />, document.getElementById('app'));
-ReactDOM.render(<ComingSoon />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
+//ReactDOM.render(<ComingSoon />, document.getElementById('app'));
